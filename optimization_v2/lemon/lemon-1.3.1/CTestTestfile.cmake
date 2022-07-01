3b6250a0cff8d9963a0a7511e951d5fdf20430b2
lucasfandre <l182495@dac.unicamp.br>
2022-07-01 09:08:33 -0300

    init

---
+++ optimization_v2/lemon/lemon-1.3.1/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/lucas/Documents/TCC/optimization_v2/lemon/lemon-1.3.1
+# Build directory: /home/lucas/Documents/TCC/optimization_v2/lemon/lemon-1.3.1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lemon")
+subdirs("contrib")
+subdirs("demo")
+subdirs("tools")
+subdirs("doc")
+subdirs("test")
---
+++ optimization_v2/lemon/lemon-1.3.1/tools/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lucas/Documents/TCC/optimization_v2/lemon/lemon-1.3.1/tools
+# Build directory: /home/lucas/Documents/TCC/optimization_v2/lemon/lemon-1.3.1/tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
