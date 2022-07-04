04dcef2b05006c043759ce769e23698d6ecbdea4
lucasfandre <l182495@dac.unicamp.br>
2022-07-04 15:39:01 -0300

    further work

--- optimization_v2/instances/python/convert.py
+++ optimization_v2/instances/python/convert.py
@@ -1,25 +1,34 @@
 import json
 import pandas as pd
 pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 data = json.load(open('data.json'))
 
 nodes = pd.DataFrame.from_dict(data['nodes'], orient='index')
 
-
-Index(['group', 'orbit', 'orbitIndex', 'out', 'in', 'skill', 'name', 'icon',
-       'ascendancyName', 'stats', 'isNotable', 'reminderText',
-       'isAscendancyStart', 'grantedStrength', 'grantedIntelligence',
-       'isProxy', 'isJewelSocket', 'expansionJewel', 'isMultipleChoiceOption',
-       'grantedPassivePoints', 'isMultipleChoice', 'isBlighted', 'recipe',
-       'grantedDexterity', 'isMastery', 'inactiveIcon', 'activeIcon',
-       'activeEffectImage', 'masteryEffects', 'isKeystone', 'flavourText',
-       'classStartIndex'],
-
+nod = nodes.columns
 nodes = nodes[nodes['isJewelSocket'] != True]
+jewels = nodes[nodes['isJewelSocket'] == True]
 nodes = nodes[nodes['isAscendancyStart'] != True]
 nodes = nodes[nodes['isBlighted']!=True]
+#blighted = nodes[nodes['isBlighted']==True]
 nodes = nodes[nodes['isMastery']!=True]
+mastery = nodes[nodes['isMastery']==True]
 nodes = nodes[nodes['ascendancyName'].isna()]
 
 nodes = nodes[['group','orbit','orbitIndex','out','in','skill','name']]
+nodes = nodes.reset_index().rename(columns={'index':'id'})
+groups = pd.DataFrame.from_dict(data['groups'], orient='index').reset_index().rename(columns={'index':'group'})
+groups['group'] = groups['group'].astype(float)
+nodes = nodes.merge(groups, on='group',how='left')
+
+#nodes.groupby(['group']).count()
+
+nodes['connect'] = nodes['out'] + nodes['in']
+position = nodes[['id','x','y','orbit','orbitIndex']]
+nodes = nodes.explode('connect')
+#in é o que entra out é oq saí
+
+nodes = nodes.drop(['out','in'], axis=1)
+nodes = nodes[~nodes['connect'].isna()]
